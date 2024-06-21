6f4239b299c3b9249a9e930e2e7f5f98ef16fbc5
Jesus Bracho <jessebracho@gmail.com>
2024-06-21 19:49:56 -0400

    refactor(loading|1386): group loading functionality

--- src/assets.rs
+++ src/assets.rs
@@ -1,25 +1,13 @@
-use crate::states::States::{Load, Play};
-use bevy::diagnostic::{DiagnosticsStore, FrameTimeDiagnosticsPlugin};
 use bevy::prelude::*;
 use bevy_asset_loader::prelude::*;
-use iyes_progress::{Progress, ProgressCounter, ProgressPlugin, ProgressSystem};
+
+use crate::states::States::Load;
 
 pub struct AssetsPlugin;
 
 impl Plugin for AssetsPlugin {
     fn build(&self, app: &mut App) {
-        app.add_plugins((
-            ProgressPlugin::new(Load).continue_to(Play),
-            FrameTimeDiagnosticsPlugin,
-        ))
-        .add_loading_state(LoadingState::new(Load).load_collection::<CharacterAssets>())
-        .add_systems(
-            Update,
-            (track_fake_long_task.track_progress(), print_progress)
-                .chain()
-                .run_if(in_state(Load))
-                .after(LoadingStateSet(Load)),
-        );
+        app.add_loading_state(LoadingState::new(Load).load_collection::<CharacterAssets>());
     }
 }
 
@@ -30,33 +18,4 @@ pub struct CharacterAssets {
 
     #[asset(texture_atlas_layout(tile_size_x = 48.0, tile_size_y = 48.0, columns = 8, rows = 24))]
     pub layout: Handle<TextureAtlasLayout>,
-}
-
-fn print_progress(
-    progress: Option<Res<ProgressCounter>>,
-    diagnostics: Res<DiagnosticsStore>,
-    mut last_done: Local<u32>,
-) {
-    if let Some(progress) = progress.map(|counter| counter.progress()) {
-        if progress.done > *last_done {
-            *last_done = progress.done;
-            info!(
-                "[Frame {}] Changed progress: {:?}",
-                diagnostics
-                    .get(&FrameTimeDiagnosticsPlugin::FRAME_COUNT)
-                    .map(|diagnostic| diagnostic.value().unwrap_or(0.))
-                    .unwrap_or(0.),
-                progress
-            );
-        }
-    }
-}
-
-fn track_fake_long_task(time: Res<Time>) -> Progress {
-    if time.elapsed_seconds_f64() > 4.0 {
-        info!("Long fake task is completed");
-        true.into()
-    } else {
-        false.into()
-    }
 }
--- src/loading.rs
+++ src/loading.rs
@@ -1,7 +1,10 @@
+use crate::states::States::{Load, Play};
 use crate::GameState;
+use bevy::diagnostic::{DiagnosticsStore, FrameTimeDiagnosticsPlugin};
 use bevy::prelude::*;
 use bevy_asset_loader::prelude::*;
 use bevy_kira_audio::AudioSource;
+use iyes_progress::{Progress, ProgressCounter, ProgressPlugin, ProgressSystem};
 
 pub struct LoadingPlugin;
 
@@ -10,6 +13,17 @@ pub struct LoadingPlugin;
 /// If interested, take a look at <https://bevy-cheatbook.github.io/features/assets.html>
 impl Plugin for LoadingPlugin {
     fn build(&self, app: &mut App) {
+        app.add_plugins((
+            ProgressPlugin::new(Load).continue_to(Play),
+            FrameTimeDiagnosticsPlugin,
+        ))
+        .add_systems(
+            Update,
+            (track_fake_long_task.track_progress(), print_progress)
+                .chain()
+                .run_if(in_state(Load))
+                .after(LoadingStateSet(Load)),
+        );
         app.add_loading_state(
             LoadingState::new(GameState::Loading)
                 .continue_to_state(GameState::Menu)
@@ -34,4 +48,33 @@ pub struct TextureAssets {
     pub bevy: Handle<Image>,
     #[asset(path = "textures/github.png")]
     pub github: Handle<Image>,
+}
+
+fn print_progress(
+    progress: Option<Res<ProgressCounter>>,
+    diagnostics: Res<DiagnosticsStore>,
+    mut last_done: Local<u32>,
+) {
+    if let Some(progress) = progress.map(|counter| counter.progress()) {
+        if progress.done > *last_done {
+            *last_done = progress.done;
+            info!(
+                "[Frame {}] Changed progress: {:?}",
+                diagnostics
+                    .get(&FrameTimeDiagnosticsPlugin::FRAME_COUNT)
+                    .map(|diagnostic| diagnostic.value().unwrap_or(0.))
+                    .unwrap_or(0.),
+                progress
+            );
+        }
+    }
+}
+
+fn track_fake_long_task(time: Res<Time>) -> Progress {
+    if time.elapsed_seconds_f64() > 4.0 {
+        info!("Long fake task is completed");
+        true.into()
+    } else {
+        false.into()
+    }
 }
